27104e3768ba2585f8f064dff95b298b8175c415
Aditya Jain <adityajain.783@gmail.com>
2017-03-05 12:53:05 +0530

    Adds fetch status component for showing errorStatus or loadingStatus while fetching user preferences from background service

--- app/components/CompetitionFixturesList/CompetitionFixturesList.jsx
+++ app/components/CompetitionFixturesList/CompetitionFixturesList.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
 import ListGroup from 'react-bootstrap/lib/ListGroup';
 
 import Header from './Header';
 import Fixture from './Fixture';
+import CustomLoading from '../commons/CustomLoading/CustomLoading';
+import ErrorAlert from '../commons/ErrorAlert/ErrorAlert';
 
 import CompetitionFixturesListCss from './CompetitionFixturesList.css';
 
+
+
+const FetchStatus = ({ status }) => {
+  return (
+    <Row>
+      <Col xs={10} xsOffset={1} md={8} mdOffset={2}>
+        <CustomLoading />
+        <ErrorAlert showStatus={true} 
+          alertMessage={"No preferences available"} 
+          buttonMessage={"Edit Preferences"} />
+      </Col>
+    </Row>
+  );
+};
 const FixturesList = ({ fixturesList }) => {
   let fixtures = fixturesList.map((fixture, index) => {
     return (<Fixture {...fixture} key={index} />);
@@ -19,6 +38,7 @@ export default class CompetitionFixturesList extends React.Component {
       <ListGroup componentClass='div'>
         <Header competition={this.props.competition} />
         <FixturesList fixturesList={this.props.fixturesList}/>
+        <FetchStatus />
       </ListGroup>
     );
   }
