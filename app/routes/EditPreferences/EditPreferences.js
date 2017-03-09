e765dcd86f35cf9e17c51a0d72edfed63f0d6daa
Aditya Jain <adityajain.783@gmail.com>
2017-03-09 15:52:12 +0530

    Adds two app routes * LatestFixturesRoute: show all latest fixtures for user preferred teams per competiton * EditPreferencesRoute: allow user to edit preferences of teams per competition

---
+++ app/routes/EditPreferences/EditPreferences.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import Grid from 'react-bootstrap/lib/Grid';
+import Glyphicon from 'react-bootstrap/lib/Glyphicon';
+import { Link } from 'react-router';
+
+import CompetitionList from '../../components/CompetitionList/CompetitionList';
+import EditPreferencesCss from './EditPreferences.css';
+
+
+
+const EditPreferencesHeader = () => {
+  return (
+    <Row className="edit-preferences-header">
+      <Col md={6} xs={6}>
+        <h2>
+          <span>Soccerify</span>
+        </h2>
+      </Col>
+      <Col md={6} xs={6}>
+        <h2 className="pull-right">
+          <Link to="/" style={{color: 'white'}}>
+            <Glyphicon glyph="calendar" />
+          </Link>
+        </h2>
+      </Col>
+    </Row>
+  );
+};
+
+export default class EditPreferences extends React.Component {
+  render() {
+    return (
+      <Grid>
+        <EditPreferencesHeader />
+        <CompetitionList competitionList={[{},{},{}]} />
+      </Grid>
+    );
+  }
+}
---
+++ app/routes/LatestFixtures/LatestFixtures.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import Grid from 'react-bootstrap/lib/Grid';
+import Glyphicon from 'react-bootstrap/lib/Glyphicon';
+import { Link } from 'react-router';
+
+import CompetitionFixturesList from '../../components/CompetitionFixturesList/CompetitionFixturesList';
+import LatestFixturesCss from './LatestFixtures.css';
+
+
+
+const LatestFixturesHeader = () => {
+  return (
+    <div>
+      <Row>
+        <Col xs={12} className="latest-fixtures-header">
+          <center><h2>Soccerify</h2></center>
+        </Col>
+      </Row>
+      <div className="divider"></div>
+      <Row className="row">
+        <Col xs={12}>
+          <h4 className="pull-right">
+            <Link to="editPreferences" style={{color: '#7986CB'}}>
+              <Glyphicon glyph="cog" />
+            </Link>
+          </h4>
+          <h4>Latest Fixtures</h4>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default class LatestFixtures extends React.Component {
+  render() {
+    return (
+      <Grid>
+        <LatestFixturesHeader />
+        <CompetitionFixturesList 
+          competition={{caption: 'Premiere League', currentMatchday: 23}}
+          fixturesList={[{},{},{}]} />
+      </Grid>
+    );
+  }
+}
